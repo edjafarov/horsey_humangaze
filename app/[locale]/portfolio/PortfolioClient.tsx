206f20f8409dff6c2a813616b557fd08d77b401c
Eldar Djafarov <djkojb@gmail.com>
2025-07-21 10:33:56 +0200

    Remove unused import for portfolioIdyllischBilingual in PortfolioClient.tsx to streamline code and improve clarity.

--- app/[locale]/portfolio/PortfolioClient.tsx
+++ app/[locale]/portfolio/PortfolioClient.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import PageContent from "@/app/components/PageContent";
 import Link from "next/link";
 import {
-  portfolioIdyllischBilingual,
   portfolioNiedlichBilingual,
   portfolioDramatischBilingual,
   portfolioHochzeitBilingual,
